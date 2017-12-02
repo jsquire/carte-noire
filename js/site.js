07c11c930158ecd23557308291e92fd9c81b9344
Jesse Squire <jesse.squire@gmail.com>
2017-12-02 13:37:19 -0500

    moved page initialization into etenral script, white restructuring for better sepratation between structure and behavior.  For now, the original approach of working with the menu was left in tact.

--- js/site.js
+++ js/site.js
@@ -1,59 +1,118 @@
-(function(root, $, undefined) {
-    // Legacy Private functions
+(function(root, window, $, undefined) {  
 
-    function getStartFontSize() {
-        try {
-            startingFontSize = parseInt($(document.getElementsByClassName("articles")[0].getElementsByTagName("a")[0]).css("font-size"));
-            setFontSize();
-            window.addEventListener('resize', setFontSize, true);
-        } 
-        catch (e) {            
-        }
+    // == Private Definitions ==
+
+    var menuEvents = {
+        open    : 'open.mm',
+        closed  : 'closed.mm'
+    };
+
+    var $window  = $(window);
+    var elements = null;
+
+    /**
+     * Determines if an object has been defined and is non-null.
+     * 
+     * @param {*} target The object to consider
+     * 
+     * @returns {bool} true if the target was defined; otherwise, false.
+     */
+    function isDefined(target) {
+        return ((target !== undefined) && (target !== null));
     }
 
-    function setFontSize() {        
-        var title, dateOfTitle, fontSizeOfTitle, listOfA, listOfSmall, listOfArticlesDiv, divWidth;
-            
-        listOfArticlesDiv = document.getElementsByClassName("articles");
-            
-        for (i = 0; i < listOfArticlesDiv.length; i++) {    
-            listOfA = document.getElementsByClassName("articles")[i].getElementsByTagName("a");
-            listOfSmall = document.getElementsByClassName("articles")[i].getElementsByTagName("small");
-      
-            divWidth = document.getElementsByClassName("articles")[i].offsetWidth;
-      
-            for (k = 0; k < listOfSmall.length; k++) {      
-                title = $(listOfA[k]);
-                dateOfTitle = $(listOfSmall[k]);
-        
-                fontSizeOfTitle = startingFontSize;
-                title.css("font-size", fontSizeOfTitle);
-        
-                while (title.width() + dateOfTitle.width() >= divWidth)
-                  title.css("font-size", fontSizeOfTitle -= 0.5);
+    // Site definition
+
+    var site = {
+
+        /**
+         * Initializes the site functionality.
+         *
+         * @param {object} settings The settings to apply to the site.  Expected members:
+         *     adjustArticleListFontSizes : A flag indicating whether article list font sizes should be adjusted or not
+         *     slideMenuSelector          : The jQuery selector for the slide menu.
+         *     slideMenuTriggerSelector   : The jQuery selector for the slide menu toggle.
+         *     articleContainerSelector   : The jQuery selector for elements contain article titles
+         *     articleTitleSelector       : The jQuery selector for elements within an Article Container that contain the article title
+         *     articleDateSelector        : The jQuery selector for elements within an Article Container that contain the article date
+         *     
+         */
+        init : function Site$init(settings) {
+            elements = {
+                sideMenu    : $(settings.slideMenuSelector),
+                menuTrigger : $(settings.slideMenuTriggerSelector)
+            };
+
+            // Initialize the sliding menu.  This makes use of the jQuery mMenu plug-in.
+           
+            var menu = elements.sideMenu.mmenu({
+                //offCanvas : false
+            });
+
+            menu.on(menuEvents.closed, function() { elements.menuTrigger.show(); });
+
+            elements.menuTrigger.click(function() {
+                elements.menuTrigger.hide();
+                menu.trigger(menuEvents.open);
+            });
+
+            // If article font sizes are to be adjusted to ensure they fit within their container, 
+            // then capture the target elements and watch for resizes to the window.
+
+            var resizeArticleTitles = function() {};
+
+            if (settings.adjustArticleListFontSizes) {
+
+                elements.articleContainers = $(settings.articleContainerSelector);
+                elements.artitleTitles     = {};
+                elements.articleDates      = {};
+
+                elements.articleContainers.each(function(index, currentElement) {
+                    var container = $(this);
+
+                    elements.artitleTitles[container] = container.find(settings.articleTitleSelector);
+                    elements.articleDates[container]  = container.find(settings.articleDateSelector);
+                });
+
+                var initialFontSize = parseInt(elements.artitleTitles[elements.articleContainers.first()].css('font-size'), 10);
+               
+                resizeArticleTitles = function() {
+                    
+                    // Ensure that the article titles and dates are sized to correctly fit within their
+                    // container elements.
+
+                    elements.articleContainers.each(function(index, currentElement) {
+                        var container      = $(this);
+                        var titles         = elements.artitleTitles[container];
+                        var dates          = elements.articleDates[container];
+                        var containerWidth = container.width();
+                        var fontSize       = initialFontSize;
+                        
+                        // Reset the font size to the default.
+
+                        titles.css('font-size', fontSize + 'px');
+
+                        // Walk back the font size until it fits within it's container.
+
+                        while (titles.width() + dates.width() >= containerWidth) {
+                            fontSize -= 0.5;
+                            titles.css('font-size', fontSize + 'px');
+                        };                        
+                    });
+                };                
             }
-        }      
-    }
 
-    root.legacygetStartFontSize = function() {
-        try {
-            startingFontSize = parseInt($(document.getElementsByClassName("articles")[0].getElementsByTagName("a")[0]).css("font-size"));
-            setFontSize();
-            window.addEventListener('resize', setFontSize, true);
-        } 
-        catch (e) {            
+            // Listen to the window resize event to control menu behavior and
+            // perform any resizing that is needed for display elements.
+
+            $window.resize(function() {
+                resizeArticleTitles();
+            });
         }
-    }
+    };
 
-    // This is the main functionality from the footer, just moved as-is.
-    root.legacyInit = function() {   
-        $("#my-menu").mmenu().on( "closed.mm", function() {
-            $(".menu-button").show();
-         });
+    // == Exports ==
 
-        $(".menu-button").click(function() {
-            $(".menu-button").hide();
-            $("#my-menu").trigger("open.mm");
-        });
-    }
-})(window, $);
+    root.Site = site;
+
+})(window, window, jQuery);
