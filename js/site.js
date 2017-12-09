2b52d9f24c955284c42f4083df6cfe6e07fed0ba
Jesse Squire <jesse.squire@gmail.com>
2017-12-09 11:30:15 -0500

    - Conversion of the side menu from toggle-for-visible to always visible when screen size allows. - Tweaks to style in order to preserve usability when the menu overlay is always-on. - Continued refactoring for separation of concerns between structure, style, and behavior. - Refactoring of site behavior script to change some globals to closures to keep scope constrainted.

--- js/site.js
+++ js/site.js
@@ -2,13 +2,9 @@
 
     // == Private Definitions ==
 
-    var menuEvents = {
-        open    : 'open.mm',
-        closed  : 'closed.mm'
-    };
-
-    var $window  = $(window);
-    var elements = null;
+    var $window                = $(window);
+    var fontSizeUnit           = 'px';
+    var defaultActiveMenuClass = 'carte-noir-active';
 
     /**
      * Determines if an object has been defined and is non-null.
@@ -46,90 +42,142 @@
     // Site definition
 
     var site = {
-
         /**
          * Initializes the site functionality.
          *
          * @param {object} settings The settings to apply to the site.  Expected members:
-         *     adjustArticleListFontSizes : A flag indicating whether article list font sizes should be adjusted or not
-         *     slideMenuSelector          : The jQuery selector for the slide menu.
-         *     slideMenuTriggerSelector   : The jQuery selector for the slide menu toggle.
-         *     articleContainerSelector   : The jQuery selector for elements contain article titles
-         *     articleTitleSelector       : The jQuery selector for elements within an Article Container that contain the article title
-         *     articleDateSelector        : The jQuery selector for elements within an Article Container that contain the article date
-         *     
+         *     adjustArticleListFontSizes : {bool}   A flag indicating whether article list font sizes should be adjusted or not         
+         *     slideMenuSelector          : {string} The jQuery selector for the slide menu.
+         *     slideMenuTriggerSelector   : {string} The jQuery selector for the slide menu toggle.
+         *     articleContainerSelector   : {string} The jQuery selector for elements contain article titles
+         *     articleTitleSelector       : {string} The jQuery selector for elements within an Article Container that contain the article title
+         *     articleDateSelector        : {string} The jQuery selector for elements within an Article Container that contain the article date
+         *     activeMenuToggleClass      : {string} [OPTIONAL] A CSS class to apply to the slide menu toggle when the menu is active
          */
         init : function Site$init(settings) {
-            elements = {
-                sideMenu    : $(settings.slideMenuSelector),
-                menuTrigger : $(settings.slideMenuTriggerSelector)
-            };
+            var sideMenu        = $(settings.slideMenuSelector);
+            var menuTrigger     = $(settings.slideMenuTriggerSelector);
+            var menuActiveClass = (settings.activeMenuToggleClass || defaultActiveMenuClass);
 
             // Initialize the sliding menu.  This makes use of the jQuery mMenu plug-in.
            
-            var menu = elements.sideMenu.mmenu({
-                //offCanvas : false
-            });
+            var menu    = sideMenu.mmenu();
+            var menuApi = menu.data('mmenu');
 
-            menu.on(menuEvents.closed, function() { elements.menuTrigger.show(); });
+            menuApi.carteMenuIsOpen = false;
 
-            elements.menuTrigger.click(function(evt) {
-                elements.menuTrigger.hide();
-                menu.trigger(menuEvents.open);
+            menuApi.carteOpenMenu = function Site$carteOpenMenu() {
+                menuApi.open();
+                menuTrigger.addClass(menuActiveClass);
+                menuApi.carteMenuIsOpen = true;
+            }
+
+            menuApi.carteCloseMenu = function Site$carteCloseMenu() {
+                menuApi.close();
+                menuTrigger.removeClass(menuActiveClass);
+                menuApi.carteMenuIsOpen = false;
+            }
+
+            menuApi.carteToggleMenu = function Site$carteToggleMenu() {
+                menuTrigger.toggleClass(menuActiveClass);
+
+                if (menuApi.carteMenuIsOpen) {
+                    menuApi.close();
+                }
+                else {
+                    menuApi.open();
+                }
                 
-                return preventeventDefault(evt);
+                menuApi.carteMenuIsOpen = (!menuApi.carteMenuIsOpen);
+            }
+
+            menuTrigger.click(function(evt) {
+                menuApi.carteToggleMenu();
+                return preventEventDefault(evt);
             });
+
+            // If the menu trigger is not visible upon initialization, ensure that the
+            // menu is open so that it can be used.
+
+            if (menuTrigger.is(':hidden')) {
+                menuApi.carteOpenMenu();
+            }
 
             // If article font sizes are to be adjusted to ensure they fit within their container, 
             // then capture the target elements and watch for resizes to the window.
 
-            var resizeArticleTitles = function() {};
+            var resizeArticleTitles = function Site$resizeArticleTitles() {};
 
             if (settings.adjustArticleListFontSizes) {
+                var articleContainers = $(settings.articleContainerSelector);
+                var artitleTitles     = {};
+                var articleDates      = {};
 
-                elements.articleContainers = $(settings.articleContainerSelector);
-                elements.artitleTitles     = {};
-                elements.articleDates      = {};
-
-                elements.articleContainers.each(function(index, currentElement) {
+                articleContainers.each(function(index, currentElement) {
                     var container = $(this);
 
-                    elements.artitleTitles[container] = container.find(settings.articleTitleSelector);
-                    elements.articleDates[container]  = container.find(settings.articleDateSelector);
+                    artitleTitles[container] = container.find(settings.articleTitleSelector);
+                    articleDates[container]  = container.find(settings.articleDateSelector);
                 });
 
-                var initialFontSize = parseInt(elements.artitleTitles[elements.articleContainers.first()].css('font-size'), 10);
+                var initialFontSize = parseInt(artitleTitles[articleContainers.first()].css('font-size'), 10);
                
-                resizeArticleTitles = function() {
-                    
+                resizeArticleTitles = function Site$resizeArticleTitles() {                    
                     // Ensure that the article titles and dates are sized to correctly fit within their
                     // container elements.
 
-                    elements.articleContainers.each(function(index, currentElement) {
+                    articleContainers.each(function() {
                         var container      = $(this);
-                        var titles         = elements.artitleTitles[container];
-                        var dates          = elements.articleDates[container];
+                        var titles         = artitleTitles[container];
+                        var dates          = articleDates[container];
                         var containerWidth = container.width();
                         var fontSize       = initialFontSize;
                         
                         // Reset the font size to the default.
 
-                        titles.css('font-size', fontSize + 'px');
+                        titles.css('font-size', fontSize + fontSizeUnit);
 
                         // Walk back the font size until it fits within it's container.
 
                         while (titles.width() + dates.width() >= containerWidth) {
                             fontSize -= 0.5;
-                            titles.css('font-size', fontSize + 'px');
+                            titles.css('font-size', fontSize + fontSizeUnit);
                         };                        
                     });
                 };                
             }
 
             // Listen to the window resize event to control menu behavior and
-            // perform any resizing that is needed for display elements.
+            // perform any resizing that is needed for display elements.  Do this only 
+            // after the resize has completed to avoid making multiple passes as the
+            // window is size is actively changing.
 
             $window.resize(function() {
+                if (this.resizeTimeout) {
+                    window.clearTimeout(this.resizeTimeout);
+                }
+
+                this.resizeTimeout = window.setTimeout(function() {
+                    $(this).trigger('resizeEnd');
+                }, 125);
+            });
+
+            $window.on('resizeEnd', function() {
+                // If there is no trigger and the menu is closed, open it.  This 
+                // indicates that the menu should be always active for a larger screen.
+                //
+                // If there is a trigger and the menu is open, close it.  This indicates
+                // that a window has been shrunk down and the user is not currently interacting
+                // with it.  In this case, the content should be maximized.
+
+                var menuTriggerVisible = menuTrigger.is(':visible');
+
+                if (((!menuTriggerVisible) && (!menuApi.carteMenuIsOpen)) ||
+                    ((menuTriggerVisible) && (menuApi.carteMenuIsOpen))) {
+                    
+                    menuApi.carteToggleMenu();
+                }
+                    
                 resizeArticleTitles();
             });
         }
